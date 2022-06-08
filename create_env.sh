334e0a5e06666bc5962b304bff88f70b298276df
Mohamed Toubar <45891807+METoubar@users.noreply.github.com>
2022-06-08 09:14:57 +0200

    Update create_env.sh

--- create_env.sh
+++ create_env.sh
@@ -3,15 +3,15 @@
 ns=${{ github.event.inputs.name }}
 size=${#ns}
 
-if (( ${{ github.event.inputs.name }} == *['!'@#\$%^\&*()_+]* && $size -lt 16 )); then
+if (( ${{ github.event.inputs.name }} == *['!'@#\$%^\&*()_+ ]* && $size -lt 16 )); then
     echo "make sure that your environment name does not contain special chars ['!'@#\$%^\&*()_+] and less than 16 character"
     echo "::set-output name=env_checks::false"
-elif (( $(kubectl get ns | grep -i ${{ github.event.inputs.team }}-${{ github.event.inputs.name }}) )); then
-    echo "This environment already exist, please make sure to delete it, or use another name"
-    echo "::set-output name=env_checks::false"
-elif ! (( $(kubectl get ns --no-header | grep -i ${{ github.event.inputs.team }} | wc -l) -lt 5 )); then
-    echo "::set-output name=env_checks::false"
-    echo "You exceeded the max limit of environments"
+# elif (( $(kubectl get ns | grep -i ${{ github.event.inputs.team }}-${{ github.event.inputs.name }}) )); then
+#     echo "This environment already exist, please make sure to delete it, or use another name"
+#     echo "::set-output name=env_checks::false"
+# elif ! (( $(kubectl get ns --no-header | grep -i ${{ github.event.inputs.team }} | wc -l) -lt 5 )); then
+#     echo "::set-output name=env_checks::false"
+#     echo "You exceeded the max limit of environments"
 else
     echo "::set-output name=env_checks::false"
 fi
